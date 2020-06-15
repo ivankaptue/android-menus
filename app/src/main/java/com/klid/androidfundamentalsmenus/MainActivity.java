2f4ab3252309d7ad32e6e039790ec69e634ab567
Ivan Kaptue <ivanokaptue@gmail.com>
2020-06-15 16:37:49 +0100

    feat: Create Action Mode Menu

--- app/src/main/java/com/klid/androidfundamentalsmenus/MainActivity.java
+++ app/src/main/java/com/klid/androidfundamentalsmenus/MainActivity.java
@@ -6,13 +6,55 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ActionMode;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActionMode mActionMode;
+
+    private ActionMode.Callback mCallback = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            getMenuInflater().inflate(R.menu.actionmode_context_menu, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.actionModelMenu1:
+                    Toast.makeText(MainActivity.this, "Action mode 1 clicked", Toast.LENGTH_SHORT).show();
+                    mode.finish();
+                    return true;
+                case R.id.actionModelMenu2:
+                    Toast.makeText(MainActivity.this, "Action mode 2 clicked", Toast.LENGTH_SHORT).show();
+                    mode.finish();
+                    return true;
+                case R.id.actionModelMenu3:
+                    Toast.makeText(MainActivity.this, "Action mode 3 clicked", Toast.LENGTH_SHORT).show();
+                    mode.finish();
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            mActionMode = null;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +63,19 @@ public class MainActivity extends AppCompatActivity {
 
         TextView tv = findViewById(R.id.txtTitle);
         registerForContextMenu(tv);
+
+        Button button = findViewById(R.id.btnActionModeMenu);
+        button.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if (mActionMode != null) {
+                    return false;
+                }
+
+                mActionMode = startSupportActionMode(mCallback);
+                return true;
+            }
+        });
     }
 
     @Override
