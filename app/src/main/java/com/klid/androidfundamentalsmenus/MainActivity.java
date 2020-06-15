40bca08f8adf1137e92e7f0799df1f5d487e1147
Ivan Kaptue <ivanokaptue@gmail.com>
2020-06-15 16:26:52 +0100

    feat: Floating context menu

--- app/src/main/java/com/klid/androidfundamentalsmenus/MainActivity.java
+++ app/src/main/java/com/klid/androidfundamentalsmenus/MainActivity.java
@@ -1,9 +1,12 @@
 package com.klid.androidfundamentalsmenus;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -15,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.txtTitle);
+        registerForContextMenu(tv);
     }
 
     @Override
@@ -39,6 +45,28 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.floating_context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.contextMenu1:
+                Toast.makeText(this, "Contextual Menu 1 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.contextMenu2:
+                Toast.makeText(this, "Contextual Menu 2 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.contextMenu3:
+                Toast.makeText(this, "Contextual Menu 3 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }
